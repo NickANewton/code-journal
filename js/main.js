32f37de27bfcbd65b0b01130977ed5ecf959014c
Nick Newton <nickanewton@gmail.com>
2022-04-04 23:35:52 +0000

    Task List 5: Added listener for photoUrl.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 /* global data */
 /* exported data */
+
+var $form = document.querySelector('form');
+var $photoUrl = document.querySelector('#photo');
+var $image = document.querySelector('img');
+
+function handleInputEvent(event) {
+  if (event.target !== $photoUrl) {
+    return;
+  }
+  $image.setAttribute('src', $photoUrl.value);
+}
+
+$form.addEventListener('input', handleInputEvent);
