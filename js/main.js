747e5c15c313449e6ed58f0550852d04c155cece
Nick Newton <nickanewton@gmail.com>
2022-04-05 06:05:07 +0000

    Task List 6, 7, 8, 9: Listen for submit, incrementing, new entries saved, remove css.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,42 @@
 /* exported data */
 
 var $form = document.querySelector('form');
-var $photoUrl = document.querySelector('#photo');
 var $image = document.querySelector('img');
+var $photo = document.querySelector('#photo');
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
 
 function handleInputEvent(event) {
-  if (event.target !== $photoUrl) {
+  if (event.target !== $photo) {
     return;
   }
-  $image.setAttribute('src', $photoUrl.value);
+  $image.setAttribute('src', $photo.value);
 }
 
+function handleSubmitEvent(event) {
+  if (event.target !== $form) {
+    return;
+  }
+  var form = {
+    title: $title.value,
+    photoUrl: $photo.value,
+    notes: $notes.value
+  };
+
+  var previousDataJSON = JSON.parse(localStorage.getItem('form-user-data'));
+
+  if (previousDataJSON === null) {
+    form.id = data.nextEntryId;
+    data.nextEntryId++;
+    data.entries.push(form);
+    localStorage.setItem('form-user-data', JSON.stringify(data));
+  } else {
+    form.id = previousDataJSON.nextEntryId;
+    previousDataJSON.nextEntryId++;
+    previousDataJSON.entries.push(form);
+    localStorage.setItem('form-user-data', JSON.stringify(previousDataJSON));
+  }
+}
+
+$form.addEventListener('submit', handleSubmitEvent);
 $form.addEventListener('input', handleInputEvent);
